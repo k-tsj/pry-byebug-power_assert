19fc2c81365b4193c20c5a420d152f1a2af83802
Kazuki Tsujimoto <kazuki@callcc.net>
2018-01-03 12:06:29 +0900

    Update copyright notices

--- lib/pry/byebug/power_assert.rb
+++ lib/pry/byebug/power_assert.rb
@@ -1,6 +1,6 @@
 # pry/byebug/power_assert.rb
 #
-# Copyright (C) 2017 Kazuki Tsujimoto, All rights reserved.
+# Copyright (C) 2017 Kazuki Tsujimoto
 
 require "pry/byebug/power_assert/version"
 require "pry/byebug/power_assert/core_ext"
